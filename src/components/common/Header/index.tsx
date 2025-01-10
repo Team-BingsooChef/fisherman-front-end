79b5dfbf1898156f53019ffaeb9eafeda9073d82
Heeyeun <koheeyeun96@gmail.com>
2025-01-10 14:59:22 +0900

    chore: Fisherman 그림 svg 삽입

---
+++ src/components/common/Header/index.tsx
@@ -0,0 +1,11 @@
+import { ChevronLeft } from "lucide-react"
+import {COLOR}  from "../../../styles/color";
+import { Flex, Text } from "@chakra-ui/react";
+
+export const BlueHeader = () => {
+    return (
+    <Flex w="100%" h="70px" justify="center" alignItems="center" backgroundColor={COLOR.PRIMARY} >
+
+    </Flex>
+    );
+};
