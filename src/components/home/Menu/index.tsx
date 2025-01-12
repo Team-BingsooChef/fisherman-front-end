85c4584cd53cebf97187bebebdc2e24646697d88
Heeyeun <koheeyeun96@gmail.com>
2025-01-13 05:50:09 +0900

    feat: 메뉴 기능 menu -> drawer로 변경

--- src/components/home/Menu/index.tsx
+++ src/components/home/Menu/index.tsx
@@ -1,136 +1,154 @@
 import {
-    Menu as CustomedMenu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    Flex,
-    Box,
-    Image
-  } from "@chakra-ui/react";
-  import { Link } from "react-router-dom";
-  import { IconButton } from "@chakra-ui/icons";
-  import { Settings, Github, Search, Fish, MenuIcon, Dice5, LogOut} from "lucide-react";
-  import profile_example from "../../../assets/profile_example.jpg";
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Flex,
+  Box,
+  Image,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { Settings, Github, Search, Fish, Dice5, LogOut, MenuIcon } from "lucide-react";
+import profile_example from "../../../assets/profile_example.jpg";
 
-  export const Menu = () => {
-    const nickName = "자고싶어핑";
-    return (
-      <Flex width="100%" justifyContent="flex-end" p={0}>
-        <CustomedMenu >
+export const DrawerMenu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const nickName = "자고싶어핑";
 
-          <MenuButton
-            as={IconButton}
-            aria-label="Options"
-            icon={<MenuIcon size="28" />}
-            variant="unstyled"
-            size="lg"
-          />
+  return (
+    <>
+      {/* 메뉴 버튼 */}
+      <Flex width="100%" justifyContent="flex-end">
+        <Button
+          onClick={onOpen}
+          variant="unstyled"
+          aria-label="Open Menu"
+        >
+          <MenuIcon size="28" />
+        </Button>
+      </Flex>
 
-          <MenuList  bg="#F9F7F7" color="white" >
-            {/* 첫 번째 MenuItem은 텍스트처럼 표시 */}
-            <MenuItem
-              isDisabled
-              fontSize="24px"
-              fontWeight="bold"
-              bg="#F9F7F7"
-              _hover={{ bg: "transparent" }}
-              _disabled={{ opacity: 1, cursor: "default", color: "#3887C7" }}
-              mb="1px"
-            >
+      {/* Drawer 컴포넌트 */}
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="xs">
+        <DrawerOverlay />
+        <DrawerContent bg="#F9F7F7">
+          <DrawerCloseButton color="#595353" />
+          <DrawerHeader>
             <Flex align="center" gap="8px">
-            <Image
-            src={profile_example}
-            alt="profile"
-            width="30px"
-            height="30px"
-            borderRadius="full"
-            objectFit="contain"
-            />
-              {nickName} 님
+              <Image
+                src={profile_example}
+                alt="profile"
+                width="30px"
+                height="30px"
+                borderRadius="full"
+                objectFit="contain"
+              />
+              <Box fontWeight="extrabold" fontSize="24px" color="#3887C7">
+                {nickName} <span style={{ color: "#595353" }}>님</span>
+              </Box>
             </Flex>
-            </MenuItem>
+          </DrawerHeader>
 
-
+          <DrawerBody>
             <Link to="/setting">
-            <MenuItem
-              icon={<Settings size="20" color="#777C89" />}
-              bg="#F9F7F7"
-              _hover={{ bg: "#AFD5F4" }}
-              color = "#595353"
-              fontWeight={700}
-            >
-            계정 설정
-            </MenuItem>
+              <Button
+                leftIcon={<Settings size="20" color="#777C89" />}
+                bg="#F9F7F7"
+                _hover={{ bg: "#AFD5F4" }}
+                color="#595353"
+                fontWeight={700}
+                width="100%"
+                justifyContent="flex-start"
+                mb="4"
+              >
+                계정 설정
+              </Button>
             </Link>
 
             <Link to="/seetoppinglist">
-            <MenuItem
-              icon={<Dice5 size="20" color="#777C89" />}
-              bg="#F9F7F7"
-              _hover={{ bg: "#AFD5F4" }}
-              color = "#595353"
-              fontWeight={700}
-            >
-              빙어 뽑기
-            </MenuItem>
-
+              <Button
+                leftIcon={<Dice5 size="20" color="#777C89" />}
+                bg="#F9F7F7"
+                _hover={{ bg: "#AFD5F4" }}
+                color="#595353"
+                fontWeight={700}
+                width="100%"
+                justifyContent="flex-start"
+                mb="4"
+              >
+                빙어 뽑기
+              </Button>
             </Link>
 
             <Link to="/search">
-            <MenuItem
-              icon={<Search size="20" color="#777C89" />}
-              bg="#F9F7F7"
-              _hover={{ bg: "#AFD5F4" }}
-              color = "#595353"
-              fontWeight={700}
-            >
-              낚시터 찾기
-            </MenuItem>
+              <Button
+                leftIcon={<Search size="20" color="#777C89" />}
+                bg="#F9F7F7"
+                _hover={{ bg: "#AFD5F4" }}
+                color="#595353"
+                fontWeight={700}
+                width="100%"
+                justifyContent="flex-start"
+                mb="4"
+              >
+                낚시터 찾기
+              </Button>
             </Link>
 
             <Link to="/seetoppinglist">
-            <MenuItem
-              bg="#F9F7F7"
-              icon={<Fish size="20" color="#777C89" />}
-              _hover={{ bg: "#AFD5F4" }}
-              color = "#595353"
-              fontWeight={700}
-            >
-              내가 보낸 빙어
-            </MenuItem>
+              <Button
+                leftIcon={<Fish size="20" color="#777C89" />}
+                bg="#F9F7F7"
+                _hover={{ bg: "#AFD5F4" }}
+                color="#595353"
+                fontWeight={700}
+                width="100%"
+                justifyContent="flex-start"
+                mb="4"
+              >
+                내가 보낸 빙어
+              </Button>
             </Link>
 
-            {/* 구분선 추가 */}
+            {/* 구분선 */}
             <Box
               as="hr"
               borderColor="gray.700"
               borderWidth="1px"
-              my={2} /* 위아래 여백 */
+              my={4}
             />
-            <MenuItem
+
+            <Button
+              leftIcon={<LogOut size="20" color="#777C89" />}
               bg="#F9F7F7"
-              icon={<LogOut size="20" color="#777C89" />}
               _hover={{ bg: "#AFD5F4" }}
-              color = "#595353"
+              color="#595353"
               fontWeight={700}
-              
+              width="100%"
+              justifyContent="flex-start"
+              mb="4"
             >
               로그아웃
-            </MenuItem>
-            <MenuItem
+            </Button>
+
+            <Button
+              leftIcon={<Github size="20" color="#777C89" />}
               bg="#F9F7F7"
-              icon={<Github size="20" color="#777C89" />}
               _hover={{ bg: "#AFD5F4" }}
-              color = "#595353"
+              color="#595353"
               fontWeight={700}
-              
+              width="100%"
+              justifyContent="flex-start"
             >
               포피셔 소개
-            </MenuItem>
-
-          </MenuList>
-        </CustomedMenu>
-      </Flex>
-    );
-  };
-  
+            </Button>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
