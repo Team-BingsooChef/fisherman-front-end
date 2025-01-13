fc34abc31b8b9a15a4828e9f719a8a64a0f9304f
Heeyeun <koheeyeun96@gmail.com>
2025-01-14 02:43:02 +0900

    feat: FishBagPage 구현

--- src/pages/user/FishBagPage.tsx
+++ src/pages/user/FishBagPage.tsx
@@ -2,21 +2,10 @@ import { WhiteLeftHeader } from "../../components/common/Header";
 import styled from "@emotion/styled";
 import { Box, SimpleGrid, Flex, Text, Container } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-
+import { fishdata } from "../../__mocks__/fish/data";
 export default function FishBagPage() {
   const navigate = useNavigate();
 
-  const data = [
-    { id: 1, color: "#D9D9D9", text: "빙어", count: 3, status: "common", image: "/src/assets/toppings/oreo.svg" },
-    { id: 2, color: "tomato", text: "송어", count: 2, status: "common", image: "/src/assets/toppings/oreo.svg" },
-    { id: 3, color: "tomato", text: "복어", count: 1, status: "common", image: "/src/assets/toppings/oreo.svg" },
-    { id: 4, color: "tomato", text: "비단잉어", count: 1, status: "rare", image: "/src/assets/toppings/oreo.svg" },
-    { id: 5, color: "tomato", text: "베타", count: 1, status: "rare", image: "/src/assets/toppings/oreo.svg" },
-    { id: 6, color: "tomato", text: "갈치", count: 1, status: "rare", image: "/src/assets/toppings/oreo.svg" },
-    { id: 7, color: "tomato", text: "황금 빙어", count: 1, status: "legendary", image: "/src/assets/toppings/oreo.svg" },
-    { id: 8, color: "tomato", text: "상어", count: 1, status: "legendary", image: "/src/assets/toppings/oreo.svg" },
-    { id: 9, color: "tomato", text: "붕어빵", count: 1, status: "funny", image: "/src/assets/toppings/oreo.svg" },
-  ];
 
   const getStatusColor = (status) => {
     switch (status) {
@@ -38,9 +27,9 @@ export default function FishBagPage() {
       <WhiteLeftHeader text="내 빙어" onBackClick={() => navigate("/fishdrawing")} />
         <Flex w="full" h="full" flexDir="column" align="center" bg="white" border={1} borderColor="gray.400" borderRadius="8px">
         <SimpleGrid columns={3} spacing="20px" mt={4}>
-          {data.map((item) => (
+          {fishdata.map((item) => (
+            <Box key={item.id}>
             <Box
-              key={item.id}
               bg="#d9d9d9"
               boxSize="100px"
               position="relative"
@@ -81,10 +70,11 @@ export default function FishBagPage() {
                 >
                 <img src={item.image} alt="fish" width="70px" height="70px" />
                 </Box>
-                <Text fontSize="14px" fontWeight="semibold" textAlign="center">
+
+                </Box>
+                <Text fontSize="14px" fontWeight="semibold" textAlign="center" mt="4px">
                   {item.text} {item.count} 개
                 </Text>
-         
             </Box>
           ))}
         </SimpleGrid>
