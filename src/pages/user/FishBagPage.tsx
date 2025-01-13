ea9a063ddbd5894ef9b19d30812043a98aa2fb93
Heeyeun <koheeyeun96@gmail.com>
2025-01-14 02:39:09 +0900

    chore: fishData mocks 추가

--- src/pages/user/FishBagPage.tsx
+++ src/pages/user/FishBagPage.tsx
@@ -7,27 +7,27 @@ export default function FishBagPage() {
   const navigate = useNavigate();
 
   const data = [
-    { id: 1, color: "#D9D9D9", text: "빙어", count: 3, status: "available" },
-    { id: 2, color: "tomato", text: "송어", count: 2, status: "available" },
-    { id: 3, color: "tomato", text: "복어", count: 1, status: "available" },
-    { id: 4, color: "tomato", text: "비단잉어", count: 1, status: "rare" },
-    { id: 5, color: "tomato", text: "베타", count: 1, status: "rare" },
-    { id: 6, color: "tomato", text: "갈치", count: 1, status: "available" },
-    { id: 7, color: "tomato", text: "황금 빙어", count: 1, status: "legendary" },
-    { id: 8, color: "tomato", text: "상어", count: 1, status: "legendary" },
-    { id: 9, color: "tomato", text: "붕어빵", count: 1, status: "funny" },
+    { id: 1, color: "#D9D9D9", text: "빙어", count: 3, status: "common", image: "/src/assets/toppings/oreo.svg" },
+    { id: 2, color: "tomato", text: "송어", count: 2, status: "common", image: "/src/assets/toppings/oreo.svg" },
+    { id: 3, color: "tomato", text: "복어", count: 1, status: "common", image: "/src/assets/toppings/oreo.svg" },
+    { id: 4, color: "tomato", text: "비단잉어", count: 1, status: "rare", image: "/src/assets/toppings/oreo.svg" },
+    { id: 5, color: "tomato", text: "베타", count: 1, status: "rare", image: "/src/assets/toppings/oreo.svg" },
+    { id: 6, color: "tomato", text: "갈치", count: 1, status: "rare", image: "/src/assets/toppings/oreo.svg" },
+    { id: 7, color: "tomato", text: "황금 빙어", count: 1, status: "legendary", image: "/src/assets/toppings/oreo.svg" },
+    { id: 8, color: "tomato", text: "상어", count: 1, status: "legendary", image: "/src/assets/toppings/oreo.svg" },
+    { id: 9, color: "tomato", text: "붕어빵", count: 1, status: "funny", image: "/src/assets/toppings/oreo.svg" },
   ];
 
   const getStatusColor = (status) => {
     switch (status) {
-      case "available":
+      case "common":
         return "green.400";
       case "rare":
-        return "blue.400";
+        return "pink.300";
       case "legendary":
-        return "yellow.400";
+        return "red.500";
       case "funny":
-        return "gray.400";
+        return "yellow.400";
       default:
         return "gray.200";
     }
@@ -36,21 +36,21 @@ export default function FishBagPage() {
   return (
     <Wrapper>
       <WhiteLeftHeader text="내 빙어" onBackClick={() => navigate("/fishdrawing")} />
-      <Container maxW="container.xl" pb={8}>
-        <SimpleGrid columns={{ base: 2, md: 3, lg: 4 }} spacing={10} mt={4}>
+        <Flex w="full" h="full" flexDir="column" align="center" bg="white" border={1} borderColor="gray.400" borderRadius="8px">
+        <SimpleGrid columns={3} spacing="20px" mt={4}>
           {data.map((item) => (
             <Box
               key={item.id}
-              bg="gray.100"
-              maxW="160px"
-              w="100%"
+              bg="#d9d9d9"
+              boxSize="100px"
               position="relative"
-              borderRadius="md"
-              overflow="hidden"
+              display="flex"
+                flexDir="column"
+                alignItems="center"
+              borderRadius="8px"
               boxShadow="sm"
               _hover={{ transform: "translateY(-2px)", boxShadow: "md" }}
               transition="all 0.2s"
-              onClick={() => console.log(`${item.text} clicked!`)}
             >
               {/* 상태 표시 */}
               <Box
@@ -63,7 +63,7 @@ export default function FishBagPage() {
                 py={1}
                 fontSize="xs"
                 fontWeight="bold"
-                borderBottomRightRadius="md"
+                borderRadius="8px"
                 zIndex="1"
                 transform="translate(-10%, -10%) rotate(-25deg)"
               >
@@ -71,28 +71,24 @@ export default function FishBagPage() {
               </Box>
 
               {/* 물고기 이미지와 정보 */}
-              <Flex direction="column" align="center" p={4}>
+          
                 <Box
-                  bg={item.color}
-                  boxSize="84px"
-                  borderRadius="full"
+                  boxSize="90px"
                   display="flex"
                   justifyContent="center"
                   alignItems="center"
-                  mb={3}
+                  
                 >
-                  <Text fontWeight="bold" color="white">
-                    {item.text}
-                  </Text>
+                <img src={item.image} alt="fish" width="70px" height="70px" />
                 </Box>
                 <Text fontSize="14px" fontWeight="semibold" textAlign="center">
                   {item.text} {item.count} 개
                 </Text>
-              </Flex>
+         
             </Box>
           ))}
         </SimpleGrid>
-      </Container>
+          </Flex>
     </Wrapper>
   );
 }
