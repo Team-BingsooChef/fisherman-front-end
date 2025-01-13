8cddc01c7a4f0c4a7c4aeba568bafe5b4c2ed142
Heeyeun <koheeyeun96@gmail.com>
2025-01-13 17:00:31 +0900

    feat: 닉네임 변경 버튼 추가

---
+++ src/components/user/ChangeNicknameModal/index.tsx
@@ -0,0 +1,38 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button
+  } from '@chakra-ui/react'
+
+interface ChangeNicknameModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+  }
+
+ export const ChangeNicknameModal = ({ isOpen, onClose }: ChangeNicknameModalProps) => {
+    return (
+      <>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant='ghost'>Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
--- src/pages/user/SettingPage.tsx
+++ src/pages/user/SettingPage.tsx
@@ -1,94 +1,144 @@
 import styled from "@emotion/styled";
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { SettingHeader } from "../../components/user/SettingHeader";
+import { useNavigate} from "react-router-dom";
 import { WhiteLeftHeader } from "../../components/common/Header";
-import { GreyInput, PasswordInput } from "../../components/user/CustomedInput";
-import { Text, Flex, Box, Image, Button, IconButton } from "@chakra-ui/react";
-import { WhiteRectangleButton } from "../../components/common/CustomedButton";
+import {ChangeNicknameModal} from "../../components/user/ChangeNicknameModal"; 
+import { Text, Flex, Box, Image, Button, IconButton, useDisclosure } from "@chakra-ui/react";
 import profile_example from "../../assets/profile_example.jpg";
-import { LockKeyhole, RotateCcw, Trash2, Pencil} from "lucide-react";
+import { LockKeyhole, RotateCcw, Trash2, Pencil } from "lucide-react";
 
 export default function SettingPage() {
   const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [currentNickname, setCurrentNickname] = useState<string>("호랭이");
+  const [changedNickname, setChangedNickname] = useState<string>("");
 
   const goHome = () => {
     navigate("/");
+  };
+  const changeNickname = () => {
+    setCurrentNickname(changedNickname);
   }
-  // 닉네임 변경 핸들러
-  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setChangedNickname(e.target.value);
-  };
-
-  // 비밀번호 변경 핸들러
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setChangedPassword(e.target.value);
-  };
 
   // 상태를 나타내는 타입
-type SearchStatus = "허용" | "금지";
+  type SearchStatus = "허용" | "금지";
 
-const [accessStatus, setAccessStatus] = useState<SearchStatus>("금지");
+  const [accessStatus, setAccessStatus] = useState<SearchStatus>("금지");
 
-const toggleAccess = () => {
-  setAccessStatus((prev) => (prev === "허용" ? "금지" : "허용"));
-};
+  const toggleAccess = () => {
+    setAccessStatus((prev) => (prev === "허용" ? "금지" : "허용"));
+  };
 
   return (
     <Wrapper>
-      <WhiteLeftHeader text="계정 설정" onBackClick={goHome}/>
-      <Flex direction="column" w="full" align="center" justify="center"  bgColor="white">
-          <Box m="20px 0 20px 0" >
-          <Box position="relative" >
-          <Image
-            src={profile_example}
-            alt="profile"
-            width="100px"
-            height="100px"
-            borderRadius="full"
-            objectFit="contain"
-          />
-          <Box position="absolute" borderRadius="50%" boxSize="30px" right="0" bottom="6px">
-          <IconButton aria-label='Edit Profile' icon={<Pencil />} borderRadius="50%" />
+      <WhiteLeftHeader text="계정 설정" onBackClick={goHome} />
+      <Flex
+        direction="column"
+        w="full"
+        align="center"
+        justify="center"
+        bgColor="white"
+      >
+        <Box m="20px 0 20px 0">
+          <Box position="relative">
+            <Image
+              src={profile_example}
+              alt="profile"
+              width="100px"
+              height="100px"
+              borderRadius="full"
+              objectFit="contain"
+            />
+            <Box
+              position="absolute"
+              borderRadius="50%"
+              boxSize="30px"
+              right="20px"
+              bottom="8px"
+            >
+              <IconButton
+              size="sm"
+                aria-label="Edit Profile"
+                icon={<Pencil size={20} />}
+                borderRadius="50%"
+              />
+            </Box>
           </Box>
-          </Box>
-    
-          <Flex  align="center" justify="center" gap="2px" fontWeight="Bold">
-          <Text fontSize="24px" color="#3887C7" >{currentNickname}</Text>
-          <Text fontSize="24px" color="black">님</Text>
-          </Flex>
 
-          </Box>
+          <Flex align="center" justify="center" gap="2px" fontWeight="Bold">
+
+            <Text fontSize="24px" color="#3887C7">
+              {currentNickname}
+            </Text>
+            <Text fontSize="24px" color="black">
+              님
+            </Text>
+            <IconButton
+                aria-label="Edit Profile"
+                icon={<Pencil size={20}/>}
+                borderRadius="50%" 
+                size="sm"
+                onClick={onOpen}
+                ml="8px"
+              />
+            <ChangeNicknameModal isOpen={isOpen} onClose={onClose} />
+          </Flex>
+        </Box>
       </Flex>
 
-
-      <Box m="15px 0 0 0" p={3} h="60px" w="full" display="flex" gap="8px" 
-      bgColor="white" alignItems="center" color="black">
-      <LockKeyhole/>
-      <FlexChangeElement text1="검색 허용" text2={accessStatus} onClick={toggleAccess} />
+      <Box
+        m="15px 0 0 0"
+        p={3}
+        h="60px"
+        w="full"
+        display="flex"
+        gap="8px"
+        bgColor="white"
+        alignItems="center"
+        color="black"
+      >
+        <LockKeyhole />
+        <FlexChangeElement
+          text1="검색 허용"
+          text2={accessStatus}
+          onClick={toggleAccess}
+        />
       </Box>
 
-    
-      <Button w="full" m="0 0 15px 0" p={3} h="60px"  display="flex" gap="8px" 
-      bgColor="white" alignItems="center" color="black" justifyContent="flex-start"  onClick={()=>navigate("/setpassword")}>
-      <RotateCcw/>
-      비밀번호 변경
+      <Button
+        w="full"
+        m="0 0 15px 0"
+        p={3}
+        h="60px"
+        display="flex"
+        gap="8px"
+        bgColor="white"
+        alignItems="center"
+        color="black"
+        justifyContent="flex-start"
+        onClick={() => navigate("/setpassword")}
+      >
+        <RotateCcw />
+        비밀번호 변경
       </Button>
 
-
-    
-      <Button w="full" p={3} h="60px"  display="flex" gap="8px" 
-      bgColor="white" alignItems="center" color="black" justifyContent="flex-start">
-      <Trash2 color="red" />
-      회원 탈퇴
+      <Button
+        w="full"
+        p={3}
+        h="60px"
+        display="flex"
+        gap="8px"
+        bgColor="white"
+        alignItems="center"
+        color="black"
+        justifyContent="flex-start"
+      >
+        <Trash2 color="red" />
+        회원 탈퇴
       </Button>
-
-      
     </Wrapper>
   );
-};
-
+}
 
 const Wrapper = styled.div`
   width: calc(100% - 60px);
@@ -105,20 +155,23 @@ const GreyTextButton = styled(Text)`
   cursor: pointer;
 `;
 
-
 interface FlexChangeElementProps {
-    text1: string;
-    text2: string;
-    onClick: () => void;
+  text1: string;
+  text2: string;
+  onClick: () => void;
 }
 
-const FlexChangeElement = ({text1,text2, onClick}: FlexChangeElementProps ) =>
-{
-    return(
-        <Flex justify="space-between" align="center" w="100%">
-    <Text color="#000000" fontWeight="600" fontSize="16px">{text1}</Text>
-    <GreyTextButton onClick={onClick}>{text2}</GreyTextButton>
+const FlexChangeElement = ({
+  text1,
+  text2,
+  onClick,
+}: FlexChangeElementProps) => {
+  return (
+    <Flex justify="space-between" align="center" w="100%">
+      <Text color="#000000" fontWeight="600" fontSize="16px">
+        {text1}
+      </Text>
+      <GreyTextButton onClick={onClick}>{text2}</GreyTextButton>
     </Flex>
-    );
-}
-
+  );
+};
