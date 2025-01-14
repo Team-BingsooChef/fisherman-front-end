1103f041a07f8dc306d6217ca605895f6d62603f
Heeyeun <koheeyeun96@gmail.com>
2025-01-14 16:45:07 +0900

    feat: 빙어 뽑기 질문 컴포넌트(MakeSureDrawing) 구현

--- src/components/owner/FishDrawing/index.tsx
+++ src/components/owner/FishDrawing/index.tsx
@@ -1,5 +1,5 @@
 import { useModalHeight } from "../../../hook/useModalHeight";
-import { useModalStateStore } from "../../../store/modal";
+import { useModalOpenStore, useModalStateStore } from "../../../store/modal";
 import {Text, Flex} from "@chakra-ui/react";
 import { BlueEllipseButton } from "../../common/CustomedButton";
 import { useCreateToppingStore } from '../../../store/api/topping/index';
@@ -27,39 +27,35 @@ export const FishDrawingResult = () => {
     );
   };
   
-  export const SelectQuizType = () => {
+  export const MakeSureDrawing = () => {
+    const { onClose } = useModalOpenStore();
     const { setModalState } = useModalStateStore();
     const { setQuizType } = useCreateToppingStore();
   
     // 퀴즈 타입 설정 후 모달 상태 변경
-    const handleSetQuizType = (type: QuizType) => {
-      setQuizType(type); // 전달받은 타입으로 quizType 업데이트
-      setModalState("makeQuizChoice"); // 모달 상태 변경
+    const handleDrawing = () => {
+      setModalState("fishDrawingResult"); // 모달 상태 변경
     };
+    const handleCancleDrawing = () => {
+      onClose();
+    }
   
-    useModalHeight("24%"); // 모달 높이 설정
+    useModalHeight("25%"); // 모달 높이 설정
   
     return (
-      <>
-        <Text color="white" fontSize="24px">퀴즈를 만드시겠습니까?</Text>
-        <Flex w="calc(100% - 90px)" gap="24px" mt="20px">
-          <BlueEllipseButton onClick={() => handleSetQuizType("Multiple")}>
-            객관식
+      <Flex w="full" flexDir="column" align="center" justify="center">
+        <Text color="#13353B" fontWeight="semibold" fontSize="20px" textAlign="center">
+          뽑기를 진행하시겠습니까? <br/>
+        코인 1개가 차감됩니다.</Text>
+        <Flex w="calc(100% - 60px)" gap="26px" mt="20px">
+          <BlueEllipseButton onClick={handleDrawing}>
+            예
           </BlueEllipseButton>
-          <BlueEllipseButton onClick={() => handleSetQuizType("OX")}>
-            O/X
+          <BlueEllipseButton onClick={handleCancleDrawing}>
+            아니요
           </BlueEllipseButton>
         </Flex>
-      </>
+      </Flex>
     );
   };
   
-
-  export const MakeSureDrawing = () => {
-    
-    return(
-        <>
-        ㅎㅇ
-        </>
-    )
-  }
