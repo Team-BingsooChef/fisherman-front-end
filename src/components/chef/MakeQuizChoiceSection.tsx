b07ae592151e27fb385099bf2f6b2d3045c17894
Heeyeun <koheeyeun96@gmail.com>
2025-01-21 14:07:31 +0900

    feat: 빙어 보내기 Section UI 완성

--- src/components/chef/MakeQuizChoiceSection.tsx
+++ src/components/chef/MakeQuizChoiceSection.tsx
@@ -9,8 +9,17 @@ import { OxQuizSection } from "./OxQuizSection";
 export const MakeQuizChoiceSection = () => {
   const navigate = useNavigate();
   const [question, setQuestion] = useState("");
+  const [quizType, setQuizType] = useState(null); // OX 또는 객관식 유형 관리
   const maxQuestionLength = 30;
   const maxOptionContentLength = 30;
+
+  const handleQuizTypeSelection = (type) => {
+    if (quizType === type) {
+      setQuizType(null); // 같은 버튼을 다시 누르면 선택 해제
+    } else {
+      setQuizType(type); // 선택된 유형 설정
+    }
+  };
 
   return (
     <>
@@ -36,6 +45,14 @@ export const MakeQuizChoiceSection = () => {
       >
         퀴즈 추가 (선택)
       </Text>
+      {!quizType && (
+        <>
+          <Text mb="20px">
+            퀴즈를 설정하고 싶지 않다면, 완료를 눌러 넘어가세요.
+          </Text>
+        </>
+      )}
+
       <Text
         fontSize="16px"
         fontWeight="bold"
@@ -47,60 +64,66 @@ export const MakeQuizChoiceSection = () => {
       </Text>
       <Flex gap="12px">
         <Button
-          bg="#D9D9D9"
+          bg={quizType === "OX" ? "#B5B5B5" : "#D9D9D9"}
           color="black"
           fontSize="20px"
           fontWeight="semiBold"
           borderRadius="16px"
           h="52px"
           w="160px"
-          onClick={() => navigate(-1)}
-          _hover={{ bg: "#D9D9D9" }} // 호버 시 배경색 변경 방지
+          onClick={() => handleQuizTypeSelection("OX")}
+          _hover={{ bg: "#B5B5B5" }}
         >
           OX
         </Button>
         <Button
-          bg="#D9D9D9"
+          bg={quizType === "Multiple" ? "#B5B5B5" : "#D9D9D9"}
           color="black"
           fontSize="20px"
           fontWeight="semiBold"
           borderRadius="16px"
           h="52px"
           w="160px"
-          onClick={() => navigate(-1)}
-          _hover={{ bg: "#D9D9D9" }} // 호버 시 배경색 변경 방지
+          onClick={() => handleQuizTypeSelection("Multiple")}
+          _hover={{ bg: "#B5B5B5" }}
         >
           객관식
         </Button>
       </Flex>
 
-      <Text
-        fontSize="16px"
-        fontWeight="bold"
-        mt="11px"
-        mb="6px"
-        w="full"
-        textAlign="left"
-      >
-        질문
-      </Text>
-      <Textarea
-        value={question}
-        onChange={(e) =>
-          setQuestion(e.target.value.slice(0, maxQuestionLength))
-        }
-        w="full"
-        h="70px"
-        bg="white"
-        border={1}
-        borderColor="gray.500"
-        borderRadius="8px"
-        placeholder="퀴즈의 질문을 작성해 주세요."
-      />
-      <Text w="full" fontSize="12px" color="black" textAlign="right">
-        {question.length}/{maxQuestionLength}
-      </Text>
-      <OxQuizSection />
+      {quizType && (
+        <>
+          <Text
+            fontSize="16px"
+            fontWeight="bold"
+            mt="11px"
+            mb="6px"
+            w="full"
+            textAlign="left"
+          >
+            질문
+          </Text>
+          <Textarea
+            value={question}
+            onChange={(e) =>
+              setQuestion(e.target.value.slice(0, maxQuestionLength))
+            }
+            w="full"
+            h="70px"
+            bg="white"
+            border={1}
+            borderColor="gray.500"
+            borderRadius="8px"
+            placeholder="퀴즈의 질문을 작성해 주세요."
+          />
+          <Text w="full" fontSize="12px" color="black" textAlign="right">
+            {question.length}/{maxQuestionLength}
+          </Text>
+          {quizType === "OX" && <OxQuizSection />}
+          {quizType === "Multiple" && <MultipleQuizSection />}
+        </>
+      )}
+
       <Flex position="absolute" bottom="23px" gap="23px">
         <Button
           bg="#D9D9D9"
@@ -111,20 +134,20 @@ export const MakeQuizChoiceSection = () => {
           h="45px"
           w="140px"
           onClick={() => navigate(-1)}
-          _hover={{ bg: "#D9D9D9" }} // 호버 시 배경색 변경 방지
+          _hover={{ bg: "#D9D9D9" }}
         >
           뒤로가기
         </Button>
         <Button
-          bg="#D9D9D9"
-          color="black"
+          bg="#03526B"
+          color="white"
           fontSize="20px"
           fontWeight="semiBold"
           borderRadius="16px"
           h="45px"
           w="140px"
           onClick={() => navigate(-1)}
-          _hover={{ bg: "#D9D9D9" }} // 호버 시 배경색 변경 방지
+          _hover={{ bg: "#03526B" }}
         >
           완료
         </Button>
