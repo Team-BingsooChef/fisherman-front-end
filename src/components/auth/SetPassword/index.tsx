a45e394f972aec10c10de898391c8553835a06bf
Heeyeun <koheeyeun96@gmail.com>
2025-02-19 15:47:10 +0900

    feat: signup api 연동

--- src/components/auth/SetPassword/index.tsx
+++ src/components/auth/SetPassword/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { PasswordInput } from "../PasswordInput";
 import { BlueRectangleButton } from "../../common/CustomedButton";
 import { useToast, Box, Flex } from "@chakra-ui/react";
@@ -22,7 +22,8 @@ export const SetPassword = () => {
       return;
     }
 
-    // 비밀번호가 동일한 경우 로직
+    localStorage.setItem("user_password", password);
+
     toast({
       title: "비밀번호가 설정되었습니다.",
       status: "success",
@@ -30,40 +31,38 @@ export const SetPassword = () => {
       isClosable: true,
     });
 
-    // 다음 단계로 이동 로직 추가
     navigate("/aftersignup");
   };
 
   return (
     <>
-    <Box w="full" marginTop="160px">
-      <PasswordInput
-        value={password}
-        text="비밀번호"
-        placeholder="10~15자, 특수문자 포함"
-        handleChange={(e) => setPassword(e.target.value)}
-      />
-      <PasswordInput
-        value={confirmPassword}
-        text="비밀번호 확인"
-        placeholder="비밀번호를 한 번 더 입력해 주세요"
-        handleChange={(e) => setConfirmPassword(e.target.value)}
-      />
-      <Box w="100%" mt="80px">
-        <BlueRectangleButton onClick={handleSubmit}>
-          회원가입
-        </BlueRectangleButton>
+      <Box w="full" marginTop="160px">
+        <PasswordInput
+          value={password}
+          text="비밀번호"
+          placeholder="10~15자, 특수문자 포함"
+          handleChange={(e) => setPassword(e.target.value)}
+        />
+        <PasswordInput
+          value={confirmPassword}
+          text="비밀번호 확인"
+          placeholder="비밀번호를 한 번 더 입력해 주세요"
+          handleChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <Box w="100%" mt="80px">
+          <BlueRectangleButton onClick={handleSubmit}>
+            회원가입
+          </BlueRectangleButton>
         </Box>
         <Flex w="full" justify="center" position="relative">
-        <Box mb="90px" position="fixed" bottom="0">
-         <img src={fisherman} alt="fisherman" width={180} height={180} />
-        </Box>
+          <Box mb="90px" position="fixed" bottom="0">
+            <img src={fisherman} alt="fisherman" width={180} height={180} />
+          </Box>
         </Flex>
       </Box>
     </>
   );
 };
-
 export const ReSetPassword = () => {
   const toast = useToast();
   const navigate = useNavigate();
@@ -95,28 +94,28 @@ export const ReSetPassword = () => {
 
   return (
     <>
-    <Box w="full" marginTop="160px">
-      <PasswordInput
-        value={password}
-        text="비밀번호"
-        placeholder="10~15자, 특수문자 포함"
-        handleChange={(e) => setPassword(e.target.value)}
-      />
-      <PasswordInput
-        value={confirmPassword}
-        text="비밀번호 확인"
-        placeholder="비밀번호를 한 번 더 입력해 주세요"
-        handleChange={(e) => setConfirmPassword(e.target.value)}
-      />
-      <Box w="100%" mt="80px">
-        <BlueRectangleButton onClick={handleSubmit}>
-          변경하기
-        </BlueRectangleButton>
+      <Box w="full" marginTop="160px">
+        <PasswordInput
+          value={password}
+          text="비밀번호"
+          placeholder="10~15자, 특수문자 포함"
+          handleChange={(e) => setPassword(e.target.value)}
+        />
+        <PasswordInput
+          value={confirmPassword}
+          text="비밀번호 확인"
+          placeholder="비밀번호를 한 번 더 입력해 주세요"
+          handleChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <Box w="100%" mt="80px">
+          <BlueRectangleButton onClick={handleSubmit}>
+            변경하기
+          </BlueRectangleButton>
         </Box>
         <Flex w="full" justify="center" position="relative">
-        <Box mb="90px" position="fixed" bottom="0">
-         <img src={fisherman} alt="fisherman" width={180} height={180} />
-        </Box>
+          <Box mb="90px" position="fixed" bottom="0">
+            <img src={fisherman} alt="fisherman" width={180} height={180} />
+          </Box>
         </Flex>
       </Box>
     </>
