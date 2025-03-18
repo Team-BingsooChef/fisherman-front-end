8a0c8b9498fd70499b884dc0d438fc0f0838a246
Heeyeun <koheeyeun96@gmail.com>
2025-03-19 01:44:11 +0900

    fix: RedirectPage 컴포넌트의 onSuccess 콜백에서 리디렉션을 처리

--- src/pages/auth/RedirectPage.tsx
+++ src/pages/auth/RedirectPage.tsx
@@ -4,7 +4,7 @@ import { AxiosResponse } from "axios";
 export default function RedirectPage() {
   const onSuccess = (data: AxiosResponse) => {
     console.log("로그인 성공:", data);
-    // 로그인 성공 후 리디렉션 로직
+
     if (data.status === 302 && data.headers.location) {
       window.location.href = data.headers.location;
     }
