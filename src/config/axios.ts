9669873a2924020b113bb45720cbb465aec190d5
Heeyeun <koheeyeun96@gmail.com>
2025-03-22 21:15:43 +0900

    fix: interceptor에403 status code도 추가

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -19,6 +19,7 @@ api.interceptors.response.use(
     if (
       error.response.status === 401 &&
       error.response.status === 500 &&
+      error.response.status === 403 &&
       !originalRequest._retry
     ) {
       originalRequest._retry = true;
