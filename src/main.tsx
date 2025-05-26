90d9009fdcc6e3b4f1ec4d8e20028cadcdf80f8a
Heeyeun <koheeyeun96@gmail.com>
2025-05-26 17:35:44 +0900

    feat: error logging을 위한 Sentry 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://aadc799b8cfb33c8d76fe2b0abb2e7c6@o4509388525207552.ingest.us.sentry.io/4509388526059520",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
