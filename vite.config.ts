c06c653b257eae19e6d3a647eb2f2d7765f3bae6
Heeyeun <koheeyeun96@gmail.com>
2025-05-27 21:28:57 +0900

    fix: map 안에 원본 content 삭제시키도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,24 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-// import basicSsl from "@vitejs/plugin-basic-ssl";
+
 export default defineConfig({
-  plugins: [// basicSsl(),
-  react({
-    jsxImportSource: "@emotion/react",
-    babel: {
-      plugins: ["@emotion/babel-plugin"],
-    },
-  }), sentryVitePlugin({
-    org: "fisherman-z4",
-    project: "javascript-react"
-  })],
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+    sentryVitePlugin({
+      org: "fisherman-z4",
+      project: "javascript-react",
+      sourcemaps: {
+        // Sentry로 업로드 후 dist에서 .map을 지움
+        filesToDeleteAfterUpload: ["./dist/**/*.map"],
+      },
+    }),
+  ],
 
   optimizeDeps: {
     include: [
@@ -29,6 +35,11 @@ export default defineConfig({
   },
 
   build: {
-    sourcemap: true
-  }
+    // 브라우저가 자동으로 못 찾게 주석을 제거하고,
+    // .map 안에는 sourcesContent를 빼도록
+    sourcemap: "hidden",
+    rollupOptions: {
+      output: { sourcemapExcludeSources: true },
+    },
+  },
 });
