8adcdd5fefcbbdf2c3f7f9e547d8fd0c9e997e03
Heeyeun <koheeyeun96@gmail.com>
2025-06-11 20:26:33 +0900

    fix: axios instacne 401 시 login redirect 로직 삭제

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -15,32 +15,6 @@ api.interceptors.response.use(
     return response;
   },
   async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      try {
-        const resp = await fetch(`${API_BASE_URL}/auth/refresh`, {
-          method: "post",
-          credentials: "include",
-        });
-
-        if (resp.ok) {
-          console.log("토큰 재발급 성공");
-          return api(originalRequest);
-        } else {
-          console.log("토큰 재발급 실패");
-          window.location.href = "/login";
-          return Promise.reject(error);
-        }
-      } catch (refreshError) {
-        console.log("토큰 재발급 요청 실패", refreshError);
-        window.location.href = "/login";
-        return Promise.reject(refreshError);
-      }
-    }
-
     if (error.response.status === 500) {
       console.log("서버 내부 오류 발생");
       // window.location.href = "/login";
