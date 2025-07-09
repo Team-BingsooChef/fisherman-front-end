7d1d6fe6f3bd320f68013d83adb95df817d4e35a
Heeyeun <koheeyeun96@gmail.com>
2025-07-08 18:50:07 -0700

    fix: modal Layout max width 조정

--- src/components/home/modal/ModalLayout.tsx
+++ src/components/home/modal/ModalLayout.tsx
@@ -54,7 +54,7 @@ const Overlay = styled.div`
 // 모달 컨테이너 스타일
 const ModalWrapper = styled.div`
   width: 100%;
-  max-width: 430px;
+  max-width: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
